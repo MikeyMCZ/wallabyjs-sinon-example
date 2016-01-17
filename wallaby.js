66f84df68dde043bd3d4a30c3494652c3e4a9441
Michal Materna <michal.materna@hotmail.com>
2016-01-17 10:31:12 +0100

    Adding sinon.js file.

--- wallaby.js
+++ wallaby.js
@@ -2,6 +2,7 @@ module.exports = function () {
     return {
         files: [
             { pattern: 'lib/require.js', instrument: false },
+            { pattern: 'lib/sinon.js', instrument: false },
             { pattern: 'lib/*.js', instrument: false, load: false },
             { pattern: 'src/*.js', load: false },
             { pattern: 'tests/test-main.js', instrument: false }
